8a20e46df54e4d8e9be85dda9dfa0ee6bbc4ffae
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-07-20 18:43:35 +0530

    loops and conditions

--- project2JS/main.js
+++ project2JS/main.js
@@ -7,3 +7,20 @@ if (true && x++) {
   console.log("inside 2nd if");
   console.log(x);
 }
+
+if (0 > 22);
+console.log("works");
+switch (00) {
+  case "0":
+  case 00:
+    console.log("lol");
+    break;
+  default:
+    console.log("000");
+}
+var val = 0;
+while (val < 3) {
+  console.log("previous" + val);
+  console.log(val++);
+  console.log("post" + val);
+}
